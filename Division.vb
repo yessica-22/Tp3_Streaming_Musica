e0ea0462bf525da2ed8188197b5c443689156664
yessica-22 <144051031+yessica-22@users.noreply.github.com>
2026-04-27 02:03:40 -0300

    chore: clean up legacy directories

--- Division.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class Division
-    Inherits Operacion
-
-    Public Overrides Function Calcular() As Single
-        Return Valor1 / Valor2
-    End Function
-End Class
--- Multiplicacion.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class Multiplicacion
-    Inherits Operacion
-
-    Public Overrides Function Calcular() As Single
-        Return Valor1 * Valor2
-    End Function
-End Class
