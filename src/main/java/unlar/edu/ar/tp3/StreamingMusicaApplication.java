e0be4bd5a53ab7fc758f996e0fa150e837758903
Yessica Esmeralda <144051031+yessica-22@users.noreply.github.com>
2026-05-14 02:56:32 -0300

    feat: elimino carpeta y archivos innecesarios

--- src/main/java/unlar/edu/ar/tp3/StreamingMusicaApplication.java
+++ src/main/java/unlar/edu/ar/tp3/StreamingMusicaApplication.java
@@ -1,5 +1,12 @@
 package unlar.edu.ar.tp3;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class StreamingMusicaApplication {
-    
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamingMusicaApplication.class, args);
+    }
 }
